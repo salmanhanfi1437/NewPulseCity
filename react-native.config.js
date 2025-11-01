c3a297c708b4661e7402cfdd9ca7dfc8817cee49
SujinZuvy <sujin@zuvy.store>
2025-11-01 16:27:15 +0530

    Update react-native.config.js

--- react-native.config.js
+++ react-native.config.js
@@ -1,42 +1,25 @@
 const path = require('path');
 
+// react-native.config.js
 module.exports = {
-  dependencies: {
-    '@react-native-async-storage/async-storage': {
-      root: path.resolve(__dirname, 'node_modules/@react-native-async-storage/async-storage'),
-    },
-    'react-native-gesture-handler': {
-      root: path.resolve(__dirname, 'node_modules/react-native-gesture-handler'),
-    },
-    'react-native-localize': {
-      root: path.resolve(__dirname, 'node_modules/react-native-localize'),
-    },
-    'react-native-pager-view': {
-      root: path.resolve(__dirname, 'node_modules/react-native-pager-view'),
-    },
-    'react-native-safe-area-context': {
-      root: path.resolve(__dirname, 'node_modules/react-native-safe-area-context'),
-    },
-    'react-native-screens': {
-      root: path.resolve(__dirname, 'node_modules/react-native-screens'),
-    },
-    'react-native-svg': {
-      root: path.resolve(__dirname, 'node_modules/react-native-svg'),
-    },
-    'react-native-vector-icons': {
-      root: path.resolve(__dirname, 'node_modules/react-native-vector-icons'),
-    },
-  },
+  dependencies: {},
   project: {
-    android: {
-      sourceDir: path.resolve(__dirname, 'android'),
-    },
-    ios: {
-      sourceDir: path.resolve(__dirname, 'ios'),
-    },
+    android: { sourceDir: './android' },
+    ios: { sourceDir: './ios' },
   },
   codegenConfig: {
     name: 'ZuvyStore',
     namespace: 'ai.zuvystore.com',
+    // Exclude libraries that don't have codegen support
+    excludedLibraries: [
+      'react-native-vector-icons',
+      'react-native-safe-area-context',
+      'react-native-gesture-handler',
+      'react-native-screens',
+      'react-native-localize',
+      'react-native-pager-view',
+      '@react-native-async-storage/async-storage',
+    ],
   },
 };
+
