9ccdfa457cfade23af9ceecefed952ebb74ab05a
SujinZuvy <sujin@zuvy.store>
2025-11-01 16:15:41 +0530

    Update react-native.config.js

--- react-native.config.js
+++ react-native.config.js
@@ -2,8 +2,29 @@ const path = require('path');
 
 module.exports = {
   dependencies: {
+    '@react-native-async-storage/async-storage': {
+      root: path.resolve(__dirname, 'node_modules/@react-native-async-storage/async-storage'),
+    },
+    'react-native-gesture-handler': {
+      root: path.resolve(__dirname, 'node_modules/react-native-gesture-handler'),
+    },
+    'react-native-localize': {
+      root: path.resolve(__dirname, 'node_modules/react-native-localize'),
+    },
+    'react-native-pager-view': {
+      root: path.resolve(__dirname, 'node_modules/react-native-pager-view'),
+    },
+    'react-native-safe-area-context': {
+      root: path.resolve(__dirname, 'node_modules/react-native-safe-area-context'),
+    },
+    'react-native-screens': {
+      root: path.resolve(__dirname, 'node_modules/react-native-screens'),
+    },
     'react-native-svg': {
       root: path.resolve(__dirname, 'node_modules/react-native-svg'),
+    },
+    'react-native-vector-icons': {
+      root: path.resolve(__dirname, 'node_modules/react-native-vector-icons'),
     },
   },
   project: {
