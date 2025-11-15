b415f676573419ae4c010215738604db97574c83
SujinZuvy <sujin@zuvy.store>
2025-11-15 17:35:00 +0530

    Create react-native.config.js

---
+++ android/app/react-native.config.js
@@ -0,0 +1,46 @@
+module.exports = {
+  dependencies: {
+    '@react-native-async-storage/async-storage': {
+      platforms: {
+        android: {
+          codegenConfig: null,
+        }
+      }
+    },
+    'lottie-react-native': {
+      platforms: {
+        android: {
+          codegenConfig: null,
+        }
+      }
+    },
+    'react-native-vector-icons': {
+      platforms: {
+        android: {
+          codegenConfig: null,
+        }
+      }
+    },
+    'react-native-localize': {
+      platforms: {
+        android: {
+          codegenConfig: null,
+        }
+      }
+    },
+    'react-native-pager-view': {
+      platforms: {
+        android: {
+          codegenConfig: null,
+        }
+      }
+    },
+    'react-native-gesture-handler': {
+      platforms: {
+        android: {
+          codegenConfig: null,
+        }
+      }
+    },
+  }
+};
